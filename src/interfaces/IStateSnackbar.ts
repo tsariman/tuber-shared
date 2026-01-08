0de3ae182c8ea66b026b2e2593f4fa372424ddee
crownlessking <riviere.king@outlook.com>
2026-01-07 22:15:51 -0500

    [Update] Eenhance IStateSnackbar interface

--- src/interfaces/IStateSnackbar.ts
+++ src/interfaces/IStateSnackbar.ts
@@ -1,17 +1,22 @@
-import IStateAnchorOrigin from './IStateAnchorOrigin';
+import { AlertProps, SnackbarProps } from '@mui/material'
+import IStateAnchorOrigin from './IStateAnchorOrigin'
 
 /**
  * Redux store snackbar state.
  */
 export default interface IStateSnackbar {
-  anchorOrigin?: IStateAnchorOrigin;
-  autoHideDuration?: number;
-  open?: boolean;
-  content?: JSX.Element;
-  message?: string;
-  actions?: JSX.Element[];
-  id?: string;
-  defaultId?: string;
-  type?: 'message' | 'customized' | 'void'; // 'message' by default
-  variant?: 'success' | 'error' | 'info' | 'warning'; // 'info' by default
+  anchorOrigin?: IStateAnchorOrigin
+  autoHideDuration?: number
+  open?: boolean
+  content?: JSX.Element | string
+  message?: string
+  actions?: JSX.Element[]
+  id?: string
+  defaultId?: string
+  type?: 'message' | 'customized' | 'void' // 'message' by default
+  variant?: 'success' | 'error' | 'info' | 'warning' // 'info' by default
+  /** Optional property to set other props for the Snackbar component */
+  props?: SnackbarProps
+  /** Optional property to set other props for the Alert component */
+  alertProps?: AlertProps
 }
