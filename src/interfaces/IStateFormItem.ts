46e9286ca3d876a8b0117ea691c99ff6d0b286fd
Riviere King <riviere.king@outlook.com>
2026-02-28 13:40:25 -0500

    [Update] Add eventPropagationEnabled property

--- src/interfaces/IStateFormItem.ts
+++ src/interfaces/IStateFormItem.ts
@@ -1,11 +1,11 @@
 
-import { CSSProperties } from 'react';
-import * as C from '../constants.client';
-import IAbstractState from './IAbstractState';
-import IStateFormItemCustom from './IStateFormItemCustom';
-import IStateLink from './IStateLink';
-import { InputProps } from '@mui/material/Input';
-import { SxProps } from '@mui/material';
+import { CSSProperties } from 'react'
+import * as C from '../constants.client'
+import IAbstractState from './IAbstractState'
+import IStateFormItemCustom from './IStateFormItemCustom'
+import IStateLink from './IStateLink'
+import { InputProps } from '@mui/material/Input'
+import { SxProps } from '@mui/material'
 
 /**
  * When adding input adornment to a text field, It's the type for `start` and
@@ -23,13 +23,13 @@ import { SxProps } from '@mui/material';
  * @see https://mui.com/material-ui/react-text-field/#input-adornments
  */
 export interface IStateFormItemAdornment {
-  type?: 'text' | 'button';
-  icon?: IStateLink;
+  type?: 'text' | 'button'
+  icon?: IStateLink
   /** Fontawesone icon @deprecated */
-  faIcon?: string;
-  text?: string;
-  textProps?: React.HTMLAttributes<HTMLSpanElement> & { sx?: SxProps };
-  [x: string]: unknown;
+  faIcon?: string
+  text?: string
+  textProps?: React.HTMLAttributes<HTMLSpanElement> & { sx?: SxProps }
+  [x: string]: unknown
 }
 
 /**
@@ -39,13 +39,13 @@ export interface IStateFormItemAdornment {
  *   // ...
  *   'inputProps': { } // <-- type for this.
  *   // ...
- * };
+ * }
  * ```
  * @see https://mui.com/material-ui/react-text-field/#input-adornments
  */
 export interface IStateFormItemInputProps extends InputProps {
-  start?: IStateFormItemAdornment;
-  end?: IStateFormItemAdornment;
+  start?: IStateFormItemAdornment
+  end?: IStateFormItemAdornment
 }
 
 /** List of all possible type a form item can be. */
@@ -97,31 +97,33 @@ export type TStateFormItemType = typeof C.BREAK_LINE
   | typeof C.TEXTFIELD
   | typeof C.TEXT_NODE
   | typeof C.TIME_PICKER
-  | typeof C.BAD_FORM_ITEM;
+  | typeof C.BAD_FORM_ITEM
 
 export default interface IStateFormItem<T=unknown> extends IAbstractState {
   /** Form field type e.g. textfield, select, radio... etc. */
-  type?: TStateFormItemType;
+  type?: TStateFormItemType
   /** Form field `id` */
-  id?: string;
+  id?: string
   /** Form field `name` */
-  name?: string;
+  name?: string
   /** Form field `value` */
-  value?: string;
-  href?: string;
-  style?: CSSProperties;
-  onClick?: unknown;
-  onFocus?: unknown;
-  onKeyDown?: unknown;
-  onChange?: unknown;
-  onBlur?: unknown;
-  label?: string;
-  highlight?: string;
-  disabled?: boolean;
+  value?: string
+  href?: string
+  style?: CSSProperties
+  onClick?: unknown
+  onFocus?: unknown
+  onKeyDown?: unknown
+  onChange?: unknown
+  onBlur?: unknown
+  label?: string
+  highlight?: string
+  disabled?: boolean
   /** Contains members that are generally not `JSX.Element` props. */
-  has?: IStateFormItemCustom<T>;
-  inputProps?: IStateFormItemInputProps;
-  items?: Array<IStateFormItem>;
+  has?: IStateFormItemCustom<T>
+  inputProps?: IStateFormItemInputProps
+  items?: Array<IStateFormItem>
   /** Disable form item */
-  disableOn?: ('click' | 'change' | 'blur' | 'error')[];
+  disableOn?: ('click' | 'change' | 'blur' | 'error')[]
+  /** When `true`, events can be handled in the form item group */
+  eventPropagationEnabled?: boolean
 }
